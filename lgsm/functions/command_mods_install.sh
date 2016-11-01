69cedaa33ced7f7c57670998cd81ab29bed7ac1b
UltimateByte <robin.labadie@gmail.com>
2016-11-01 19:09:22 +0100

    file init

---
+++ lgsm/functions/command_mods_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# LGSM command_mods_install.sh function
+# Author: Daniel Gibbs
+# Contributor: UltimateByte
+# Website: https://gameservermanagers.com
+# Description: List and installs available mods.
+
+local commandname="MODS"
+local commandaction="Install_Mods"
+local function_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))"
+
+check.sh
