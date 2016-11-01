71378a012c133a2964370c0777ffb623460cae99
UltimateByte <robin.labadie@gmail.com>
2016-11-01 18:51:40 +0100

    file init

---
+++ lgsm/functions/mods_list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# LGSM mods_available.sh function
+# Author: Daniel Gibbs
+# Contributor: UltimateByte
+# Website: https://gameservermanagers.com
+# Description: List available mods for different games
+
+local commandname="MODS"
+local commandaction="List Mods"
+local function_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))"
+
+check.sh
+
+if [ "${engine}" == "source" ]&&[ "${gamename} != "Garry's Mod" ]; then
+fi
